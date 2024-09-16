0362172ff90c3ee7e92dfe2cd0f7df72a260a7f0
Yucheng-An <yuc.an2019@gmail.com>
2024-09-15 23:44:33 -0700

    Binary Search Tree

--- python/binary-search-tree/binary_search_tree.py
+++ python/binary-search-tree/binary_search_tree.py
@@ -1,8 +1,8 @@
 class TreeNode:
     def __init__(self, data, left=None, right=None):
-        self.data = None
-        self.left = None
-        self.right = None
+        self.data = data
+        self.left = left
+        self.right = right
 
     def __str__(self):
         return f'TreeNode(data={self.data}, left={self.left}, right={self.right})'
@@ -10,10 +10,37 @@ class TreeNode:
 
 class BinarySearchTree:
     def __init__(self, tree_data):
-        pass
+        self.root = None
+        for value in tree_data:
+            self.insert(value)
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = TreeNode(value)
+        else:
+            self._insert_recursive(self.root, value)
+
+    def _insert_recursive(self, node, value):
+        if value <= node.data:
+            if node.left is None:
+                node.left = TreeNode(value)
+            else:
+                self._insert_recursive(node.left, value)
+        else:
+            if node.right is None:
+                node.right = TreeNode(value)
+            else:
+                self._insert_recursive(node.right, value)
 
     def data(self):
-        pass
+        return self.root
 
     def sorted_data(self):
-        pass
+        return self._in_order_traversal(self.root)
+
+    def _in_order_traversal(self, node):
+        if node is None:
+            return []
+        return (self._in_order_traversal(node.left) +
+                [node.data] +
+                self._in_order_traversal(node.right))
